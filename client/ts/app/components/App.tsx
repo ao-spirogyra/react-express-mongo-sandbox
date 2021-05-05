4247c1a2aa662931d51c0059b34a71df10e5c42c
ao-spirogyra <lily.spirogyra@gmail.com>
2021-05-05 10:03:47 +0900

    delete console.log for debug

--- client/ts/app/components/App.tsx
+++ client/ts/app/components/App.tsx
@@ -14,8 +14,6 @@ export const App: React.FC = () => {
     };
     get();
   }, []);
-  console.log(response)
-  console.log(process.env.NODE_ENV)
   return (
     <>
       <h1>Hello Typescript-React!</h1>
